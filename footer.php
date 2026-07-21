7589db33b279af52f6b98c4123a5f403e395a97f
Wordpress DEV (AHX) <alexander@familie-herbst.net>
2026-07-21 21:18:02 +0200

    Add backend link for logged-in users in footer privacy section

--- footer.php
+++ footer.php
@@ -5,6 +5,9 @@
 
     <?php if ((bool) get_theme_mod('ahx_privacy_enable_link', 1)) : ?>
         <p class="footer-privacy-link">
+            <?php if (is_user_logged_in()) : ?>
+                <a href="<?php echo esc_url(admin_url()); ?>"><?php echo esc_html__('Backend', 'ahx_wp_lean'); ?></a> |
+            <?php endif; ?>
             <a href="<?php echo esc_url(ahx_get_privacy_policy_url()); ?>"><?php echo esc_html__('Datenschutz', 'ahx_wp_lean'); ?></a>
         </p>
     <?php endif; ?>
